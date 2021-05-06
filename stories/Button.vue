6a7e3f9c095ef4e1dfd36252440af70ac5dd53fe
Ryan Taylor <ryan@snyk.io>
2021-05-06 18:49:41 +0100

    Initial commit

---
+++ stories/Button.vue
@@ -0,0 +1,58 @@
+<template>
+  <button type="button" :class="classes" @click="onClick" :style="style">{{ obj.key }}</button>
+</template>
+
+<script>
+import './button.css';
+
+export default {
+  name: 'my-button',
+
+  props: {
+    obj: {
+      type: Object,
+      default: null,
+    },
+    label: {
+      type: String,
+      default: null,
+    },
+    primary: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      default: 'medium',
+      validator: function (value) {
+        return ['small', 'medium', 'large'].indexOf(value) !== -1;
+      },
+    },
+    backgroundColor: {
+      type: String,
+    },
+  },
+
+  computed: {
+    classes() {
+      return {
+        'storybook-button': true,
+        'storybook-button--primary': this.primary,
+        'storybook-button--secondary': !this.primary,
+        [`storybook-button--${this.size}`]: true,
+      };
+    },
+    style() {
+      return {
+        backgroundColor: this.backgroundColor,
+      };
+    },
+  },
+
+  methods: {
+    onClick() {
+      this.$emit('onClick');
+    },
+  },
+};
+</script>
